90fb1fe0b9800f15f8b98e99673078a088ba8870
Sepr0cas <150806993+Sepr0cas@users.noreply.github.com>
2025-01-31 09:32:32 +0100

    dnbm

--- Multiplayerr/Assets/Scripts/StartSceneMenu.cs
+++ Multiplayerr/Assets/Scripts/StartSceneMenu.cs
@@ -3,29 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 public class StartSceneMenu : MonoBehaviour
 {
+    public GameObject checkCharac;
+    public TextMeshProUGUI warningText;
+
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        warningText.gameObject.SetActive(false);
     }
 
     public void ChooseCharacter(GameObject character)
     {
         GameManager.Instance.SelectedCharacter = character;
+        checkCharac = character;
     }
 
     public void MainMenu()
     {
         SceneManager.LoadScene(0);
     }
+
     public void PlayGame()
     {
+        if (checkCharac == null)
+        {
+            warningText.text = "Please select a Character";
+            warningText.gameObject.SetActive(true);
+            return;
+        }
         SceneManager.LoadScene(1);
     }
 
